964126395547a59540de4afa01a27ba1bafe475b
vaziria <manorder123@gmail.com>
2024-07-24 16:16:14 +0700

    local server

--- cmd/warehouse/main.go
+++ cmd/warehouse/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"path/filepath"
 	"time"
 
 	"github.com/pdcgo/pdc_orc/pkg/golang_proj"
@@ -11,10 +12,13 @@ import (
 func main() {
 	frontend := node_proj.NewNodeProject(
 		&node_proj.GitConfig{
-			Uri: "https://github.com/PDC-Repository/warehois.git",
+			Uri:    "https://github.com/PDC-Repository/warehois.git",
+			Branch: "master",
 		},
 		"/tmp/warehouse_frontend",
-	)
+	).
+		Check().
+		Pulling()
 	backend := golang_proj.
 		NewGolangProject(
 			&golang_proj.GitConfig{
@@ -25,15 +29,16 @@ func main() {
 		).
 		Check().
 		SetEnvs(golang_proj.MapEnvs{
-			"DB_DRIVER":       "sqlite",
-			"DB_SQLITE_FNAME": "/tmp/dev_db.sqlite",
-			"WAREHOUSE_HOST":  "localhost",
-			"DEV_MODE":        "true",
+			"DB_DRIVER":           "sqlite",
+			"DB_SQLITE_FNAME":     "/tmp/dev_db.sqlite",
+			"WAREHOUSE_HOST":      "localhost",
+			"DEV_MODE":            "true",
+			"SDK_FILE":            filepath.Join(frontend.Dir, "client", "src/api/api-sdk.ts"),
+			"SDK_FILE_PERMISSION": filepath.Join(frontend.Dir, "client", "src/api/permission.ts"),
+			"SDK_TEMPLATE":        filepath.Join(frontend.Dir, "client", "src/api/api-sdk.template"),
 		})
 
 	client := frontend.
-		Check().
-		Pulling().
 		Folder("client").
 		InstallPackage().
 		DotEnv(map[string]string{
@@ -44,8 +49,16 @@ func main() {
 		})
 
 	admin := frontend.
-		Check().
-		Pulling().
+		Folder("client").
+		InstallPackage().
+		DotEnv(map[string]string{
+			"VITE_BASE_URL":            "http://localhost:8080",
+			"VITE_RECAPTCHA_TOKEN":     "",
+			"VITE_CLIENT_URL":          "http://localhost:5173",
+			"VITE_ADMIN_WAREHOUSE_URL": "http://localhost:5174",
+		})
+
+	warehouse := frontend.
 		Folder("admin").
 		InstallPackage().
 		DotEnv(map[string]string{
@@ -58,7 +71,8 @@ func main() {
 	}
 
 	go client.PackageRunScript("dev", "--port", "5173")
-	go admin.PackageRunScript("dev", "--port", "5174")
+	go warehouse.PackageRunScript("dev", "--port", "5174")
+	go admin.PackageRunScript("dev-admin", "--port", "5175")
 	backend.Run("go", "run", ".\\cmd\\app_server", "dev")
 	// backend.Run(".\\shell\\run_devel.bat")
 
